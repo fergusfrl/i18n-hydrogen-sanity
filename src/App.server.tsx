fb382666993278a3b0d87fb581e7334b25a9c6ed
Fergus Farrell <fergus@batch.nz>
2022-07-09 15:56:52 +1200

    removed server ip locator

--- src/App.server.tsx
+++ src/App.server.tsx
@@ -6,7 +6,6 @@ import {
   Router,
   ShopifyAnalytics,
   ShopifyProvider,
-  useQuery,
   type HydrogenRouteProps,
 } from '@shopify/hydrogen';
 import renderHydrogen from '@shopify/hydrogen/entry-server';
@@ -18,29 +17,13 @@ import LoadingFallback from './components/global/LoadingFallback';
 import NotFound from './components/global/NotFound.server';
 import {DEFAULT_ISO_CODE} from './constants';
 
-import requestIp from 'request-ip';
-
-function App({request, response}: HydrogenRouteProps) {
+function App({request}: HydrogenRouteProps) {
   const pathname = new URL(request.normalizedUrl).pathname;
   const localeMatch = /^\/([a-z]{2})(\/|$)/i.exec(pathname);
   const countryCode = localeMatch ? (localeMatch[1] as CountryCode) : undefined;
 
-  const {data} = useQuery(['geo', 'value'], async () => {
-    const ip = requestIp.getClientIp(request) || '207.97.227.239';
-    const geo = await (
-      await fetch(`http://ip-api.com/json/${ip}?fields=countryCode`)
-    ).json();
-    return geo;
-  });
-
-  if (data.countryCode && data.countryCode !== countryCode) {
-    // TODO: relocated on client - not on server.
-    response.redirect(`/${data.countryCode.toLowerCase()}/`);
-  }
-
   return (
     <Suspense fallback={<LoadingFallback />}>
-      <p>{data.countryCode}</p>
       <ShopifyProvider countryCode={countryCode || DEFAULT_ISO_CODE}>
         <ServerCartProvider countryCode={countryCode || DEFAULT_ISO_CODE}>
           <DefaultSeo />
